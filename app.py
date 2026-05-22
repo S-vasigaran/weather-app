4c50c9540c9defea4486f3fac683e04e117b587f
S-vasigaran <115130249+S-vasigaran@users.noreply.github.com>
2026-05-22 13:22:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import weather_app as wa
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    weather = None
+    if request.method == "POST":
+        city = request.form.get("city")
+        weather = wa.get_current_weather(city)  # must return dict
+    return render_template("index.html", weather=weather)
+
+if __name__ == "__main__":
+    app.run(debug=True)
