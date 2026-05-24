72f58ba7f733be501b79d3def87ba2db67670772
S-vasigaran <115130249+S-vasigaran@users.noreply.github.com>
2026-05-24 19:34:47 +0530

    Add 5days forecast

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     weather = None
+    forecast = None
     if request.method == "POST":
         city = request.form.get("city")
-        weather = wa.get_current_weather(city)  # must return dict
-    return render_template("index.html", weather=weather)
+        weather = wa.get_current_weather(city)
+        forecast = wa.get_5day_forecast(city)
+    return render_template("index.html", weather=weather, forecast=forecast)
 
 if __name__ == "__main__":
     app.run(debug=True)
